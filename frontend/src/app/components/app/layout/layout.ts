d8f04dcb80e6a1d52bdec2b980c890c114871530
oussamasine07 <sineoussama@gmail.com>
2025-07-01 17:53:42 +0100

    make main dashboard patcials

--- frontend/src/app/components/app/layout/layout.ts
+++ frontend/src/app/components/app/layout/layout.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {SideNav} from '../partials/side-nav/side-nav';
+import {Navbar} from '../partials/navbar/navbar';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [
+    SideNav,
+    Navbar
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
