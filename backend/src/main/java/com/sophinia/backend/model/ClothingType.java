73b596fe9c630467e3797041e401c39fa2f5847b
oussamasine07 <sineoussama@gmail.com>
2025-07-09 10:52:09 +0100

    create the entity model for clothing type

---
+++ backend/src/main/java/com/sophinia/backend/model/ClothingType.java
@@ -0,0 +1,66 @@
+package com.sophinia.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clothing_types")
+public class ClothingType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "image", nullable = true)
+    private String image;
+
+    public ClothingType () {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
