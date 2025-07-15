a247d3f80850ca441b0002c52bdad22756840b66
oussamasine07 <sineoussama@gmail.com>
2025-07-15 17:48:29 +0100

    create model MeasurementField model

---
+++ backend/src/main/java/com/sophinia/backend/model/MeasurementField.java
@@ -0,0 +1,33 @@
+package com.sophinia.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "measurement_fields")
+public class MeasurementField {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public MeasurementField () {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
