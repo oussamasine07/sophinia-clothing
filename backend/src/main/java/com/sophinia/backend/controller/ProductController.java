d1896519d0c4c3a77debbf26d7e424e842eb45e4
oussamasine07 <sineoussama@gmail.com>
2025-07-21 18:59:17 +0100

    finish product CRUD

--- backend/src/main/java/com/sophinia/backend/controller/ProductController.java
+++ backend/src/main/java/com/sophinia/backend/controller/ProductController.java
@@ -10,13 +10,11 @@ import com.sophinia.backend.service.MeasurementFieldService;
 import com.sophinia.backend.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/product")
@@ -36,6 +34,10 @@ public class ProductController {
         this.measurementFieldService = measurementFieldService;
     }
 
+    @GetMapping
+    public ResponseEntity<?> index () {
+        return productService.getAllProducts();
+    }
 
     @PostMapping
     public ResponseEntity<?> create ( @Valid @RequestBody ProductValidationDTO productValidationDTO) {
@@ -62,24 +64,78 @@ public class ProductController {
                             .getMeasurementFieldById( id )
                             .getBody();
                 })
-                .toList();
+                .collect(Collectors.toCollection(ArrayList::new));
 
-        productValidationDTO.measurement_fields()
-                .forEach(measure -> {
-                    MeasurementField newMeasure = new MeasurementField();
-                    newMeasure.setName( measure.name() );
+        if (productValidationDTO.measurement_fields() != null) {
+            productValidationDTO.measurement_fields()
+                    .forEach(measure -> {
+                        MeasurementField newMeasure = new MeasurementField();
+                        newMeasure.setName( measure.name() );
 
+                        MeasurementField savedMeasure = (MeasurementField) measurementFieldService
+                                .createMeasurementField( newMeasure )
+                                .getBody();
 
-                    MeasurementField savedMeasure = (MeasurementField) measurementFieldService
-                            .createMeasurementField( newMeasure )
-                            .getBody();
-
-                    measurementFields.add( savedMeasure );
-                });
+                        measurementFields.add( savedMeasure );
+                    });
+        }
 
         product.setProductMeasurementFields( measurementFields );
 
         return productService.createProduct( product );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update (
+            @Valid @RequestBody ProductValidationDTO productValidationDTO,
+            @PathVariable Long id
+    ) {
+        Product product = new Product();
+
+        product.setName(productValidationDTO.name());
+        product.setDescription(productValidationDTO.description());
+
+        // get clothing type
+        ClothingType clothingType = (ClothingType) clothingTypeService
+                .getClothingTypeById( productValidationDTO.clothing_type() )
+                .getBody();
+        product.setClothingType( clothingType );
+
+        // setup measurements
+        List<MeasurementField> measurementFields = productValidationDTO.measurements_fields_ids() == null
+                ? new ArrayList<>()
+                :  productValidationDTO
+                .measurements_fields_ids()
+                .stream()
+                .map(mId -> {
+                    return (MeasurementField) measurementFieldService
+                            .getMeasurementFieldById( mId )
+                            .getBody();
+                })
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        if (productValidationDTO.measurement_fields() != null) {
+            productValidationDTO.measurement_fields()
+                    .forEach(measure -> {
+                        MeasurementField newMeasure = new MeasurementField();
+                        newMeasure.setName( measure.name() );
+
+                        MeasurementField savedMeasure = (MeasurementField) measurementFieldService
+                                .createMeasurementField( newMeasure )
+                                .getBody();
+
+                        measurementFields.add( savedMeasure );
+                    });
+        }
+
+        product.setProductMeasurementFields( measurementFields );
+
+        return productService.updateProduct( product, id );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete ( @PathVariable Long id ) {
+        return productService.deleteProduct( id );
     }
 
 }
--- backend/src/main/java/com/sophinia/backend/service/ProductService.java
+++ backend/src/main/java/com/sophinia/backend/service/ProductService.java
@@ -1,11 +1,16 @@
 package com.sophinia.backend.service;
 
 
+import com.sophinia.backend.exception.NotFoundException;
 import com.sophinia.backend.model.Product;
 import com.sophinia.backend.repository.ProductRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductService {
@@ -18,9 +23,39 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    public ResponseEntity<?> getAllProducts () {
+        List<Product> products = productRepository.findAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 
     public ResponseEntity<?> createProduct (Product product) {
         return new ResponseEntity<>( productRepository.save(product), HttpStatus.OK );
+    }
+
+
+    public ResponseEntity<?> updateProduct (Product product, Long id) {
+        Product updatedProduct = productRepository.findById( id )
+                .orElseThrow(() -> new NotFoundException("this product not found"));
+
+        updatedProduct.setName( product.getName() );
+        updatedProduct.setDescription( product.getDescription() );
+        updatedProduct.setClothingType( product.getClothingType() );
+        updatedProduct.setProductMeasurementFields( product.getProductMeasurementFields() );
+
+        return new ResponseEntity<>( productRepository.save( updatedProduct ), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> deleteProduct ( Long id ) {
+
+        Product deletedProduct = productRepository.findById( id )
+                .orElseThrow(() -> new NotFoundException("this product not found"));
+
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "decoration " + deletedProduct.getName() + " removed");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
     }
 
 }
