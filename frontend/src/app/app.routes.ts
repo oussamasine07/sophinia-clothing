12b282f2d3aff14d5825a4502aeff60af7705a19
oussamasine07 <sineoussama@gmail.com>
2025-07-29 18:36:59 +0100

    start with login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {Index} from './components/pages/index';
 import {Layout} from './components/app/layout/layout';
+import {Admin} from './components/app/auth/admin/admin';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: "app",
     component: Layout
+  },
+  {
+    path: "login/admin",
+    component: Admin
   }
 ];
