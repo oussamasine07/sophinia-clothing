ae7de655768a488b80c28b6fc0b2c0ce9015f1e7
oussamasine07 <sineoussama@gmail.com>
2025-08-01 00:10:24 +0100

    update redirect routes in the front

--- frontend/src/app/components/app/layout/layout.ts
+++ frontend/src/app/components/app/layout/layout.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {SideNav} from '../partials/side-nav/side-nav';
 import {Navbar} from '../partials/navbar/navbar';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -11,6 +12,15 @@ import {Navbar} from '../partials/navbar/navbar';
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
-export class Layout {
+export class Layout implements OnInit {
+
+  router: Router = inject(Router);
+  token: string | null = localStorage.getItem('token');
+
+  ngOnInit () {
+    if (!this.token) {
+      this.router.navigate(["/login"])
+    }
+  }
 
 }
