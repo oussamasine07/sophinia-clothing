7d3df0f72b410ee17e5d639e7fe31d9a93e95f6f
oussamasine07 <sineoussama@gmail.com>
2025-08-11 15:01:57 +0100

    add the uploads folder in the backend and ignore it

--- backend/src/main/java/com/sophinia/backend/bean/FileUpload.java
+++ backend/src/main/java/com/sophinia/backend/bean/FileUpload.java
@@ -14,6 +14,7 @@ public class FileUpload {
 
         // 1️⃣ Ensure the directory path is correct
         String uploadDir = System.getProperty("user.dir")
+                + File.separator + "backend"
                 + File.separator + "uploads"
                 + File.separator + dir;
 
