c3b45e35b0e2bebdfa3c7d111583843bb10e8a0d
oussamasine07 <sineoussama@gmail.com>
2025-08-20 18:47:40 +0100

    add image upload to clothing type

--- backend/src/main/java/com/sophinia/backend/dto/validation/ClothingTypeValidationDTO.java
+++ backend/src/main/java/com/sophinia/backend/dto/validation/ClothingTypeValidationDTO.java
@@ -1,11 +1,14 @@
 package com.sophinia.backend.dto.validation;
 
+import com.sophinia.backend.validation.FileSize;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.multipart.MultipartFile;
 
 public record ClothingTypeValidationDTO(
         @NotBlank(message = "name field is required")
         String name,
 
-        String image
+        @FileSize(max = 5 * 1024 * 1024, message = "File too large")
+        MultipartFile image
 ) {
 }
