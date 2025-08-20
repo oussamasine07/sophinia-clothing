104fceb600f85ccd162d03b3edd5cea9919a5e70
oussamasine07 <sineoussama@gmail.com>
2025-08-20 19:48:16 +0100

    show list of all clothing types

--- frontend/src/app/components/app/content/clothing-type/clothing-type-list/clothing-type-list.ts
+++ frontend/src/app/components/app/content/clothing-type/clothing-type-list/clothing-type-list.ts
@@ -1,10 +1,13 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, Input, OnInit} from '@angular/core';
 import {ClothingTypeService} from '../../../../../services/clothing-type/clothing-type-service';
 import {ClothingTypeInterface} from '../../../../../models/interfaces/clothing-type-interface';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-clothing-type-list',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './clothing-type-list.html',
   styleUrl: './clothing-type-list.css'
 })
@@ -22,5 +25,48 @@ export class ClothingTypeList implements OnInit {
     })
   }
 
+
+  showCreateModal = false;
+  openCreateModal () {
+    this.showCreateModal = true;
+  }
+  closeCreateModal () {
+    this.showCreateModal = false
+  }
+
+  addNewClothingType (design: ClothingTypeInterface) {
+    this.clothingTypes.push( design );
+  }
+
+  @Input() currentClothingType: ClothingTypeInterface | null = null
+  showUpdateModal = false;
+  openUpdateModal (design: ClothingTypeInterface) {
+    this.showUpdateModal = true;
+    this.currentClothingType = design;
+  }
+  closeUpdateModal () {
+    this.showUpdateModal = false;
+  }
+  updateClothingType (design: ClothingTypeInterface) {
+    this.clothingTypes = this.clothingTypes.map((d: ClothingTypeInterface) => d.id == design.id ? design : d);
+  }
+
+  @Input() currentDeleteType: string = "";
+  showDeleteModal = false;
+  openDeleteModal ( d: ClothingTypeInterface ) {
+    this.showDeleteModal = true;
+    this.currentClothingType = d;
+    this.currentDeleteType = "clothing-type";
+  }
+  closeDeleteModal () {
+    this.showDeleteModal = false;
+    this.currentClothingType = null;
+  }
+  deleteModal ( d: ClothingTypeInterface ) {
+    this.clothingTypes = this.clothingTypes.filter( ct => ct.id != d.id)
+    console.log( d )
+    console.log( this.clothingTypes )
+    this.closeDeleteModal()
+  }
 
 }
