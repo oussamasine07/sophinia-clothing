482f3e302b014a6be06ff3ce8fa42344dbcc2a8e
oussamasine07 <sineoussama@gmail.com>
2025-09-13 20:11:00 +0100

    make layout responsive

--- frontend/src/app/components/app/partials/side-nav/side-nav.ts
+++ frontend/src/app/components/app/partials/side-nav/side-nav.ts
@@ -1,51 +1,61 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-side-nav',
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './side-nav.html',
   styleUrl: './side-nav.scss',
   standalone: true
 })
-export class SideNav implements AfterViewInit {
+export class SideNav {
 
-  @ViewChild("asideComp") asideCompRef!: ElementRef<HTMLDivElement>;
-  @ViewChild("logoContainer") logoContainerRef!: ElementRef<HTMLDivElement>;
-  @ViewChild("logo") logoRef!: ElementRef<HTMLImageElement>;
-  @ViewChild("nav") navRef!: ElementRef<HTMLDivElement>;
+  @Input() isOpen = false;          // from layout
+  @Output() closeSidebar = new EventEmitter<void>();
 
-
-
-  navHeight: any = 0;
-
-  ngAfterViewInit () {
-
-    const logo = this.logoRef.nativeElement;
-    const aside = this.asideCompRef.nativeElement;
-    const logoContainer = this.logoContainerRef.nativeElement
-
-    const calculateRest = () => {
-      const asideHeight = aside.offsetHeight;
-      const logoHeight = logo.offsetHeight;
-      const logoStyles = window.getComputedStyle( logoContainer );
-      const logoMarginBottom = parseInt(logoStyles.marginBottom)
-      this.navHeight = asideHeight - logoHeight - logoMarginBottom - 60;
-
-      console.log('asideHeight', asideHeight);
-      console.log('logoHeight', logoHeight);
-      console.log('the rest is ', this.navHeight)
-    };
-
-    if (logo.complete) {
-      // image already loaded
-      calculateRest();
-    } else {
-      // wait for image to load
-      logo.onload = () => calculateRest();
-    }
+  close() {
+    this.closeSidebar.emit();
   }
 
 }
+
+
+// @ViewChild("asideComp") asideCompRef!: ElementRef<HTMLDivElement>;
+// @ViewChild("logoContainer") logoContainerRef!: ElementRef<HTMLDivElement>;
+// @ViewChild("logo") logoRef!: ElementRef<HTMLImageElement>;
+// @ViewChild("nav") navRef!: ElementRef<HTMLDivElement>;
+
+
+
+// navHeight: any = 0;
+//
+// ngAfterViewInit () {
+//
+//   const logo = this.logoRef.nativeElement;
+//   const aside = this.asideCompRef.nativeElement;
+//   const logoContainer = this.logoContainerRef.nativeElement
+//
+//   const calculateRest = () => {
+//     const asideHeight = aside.offsetHeight;
+//     const logoHeight = logo.offsetHeight;
+//     const logoStyles = window.getComputedStyle( logoContainer );
+//     const logoMarginBottom = parseInt(logoStyles.marginBottom)
+//     this.navHeight = asideHeight - logoHeight - logoMarginBottom - 60;
+//
+//     console.log('asideHeight', asideHeight);
+//     console.log('logoHeight', logoHeight);
+//     console.log('the rest is ', this.navHeight)
+//   };
+//
+//   if (logo.complete) {
+//     // image already loaded
+//     calculateRest();
+//   } else {
+//     // wait for image to load
+//     logo.onload = () => calculateRest();
+//   }
+// }
