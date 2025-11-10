4c9ab5875aa101746bdc69f885537c07edb7d04e
oussamasine07 <sineoussama@gmail.com>
2025-11-10 12:16:05 +0100

    add login functionality for client

--- frontend/src/app/components/client/pages/login/login.ts
+++ frontend/src/app/components/client/pages/login/login.ts
@@ -1,18 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {Header} from "../../partials/header/header";
-import {RemoveDashPipe} from "../../../../pipes/remove-dash-pipe";
 import {Footer} from '../../partials/footer/footer';
+import {Auth} from '../../../../services/auth/auth';
+import {Router} from '@angular/router';
+import {loginFromType} from '../../../../models/types/loginFormType';
+import {FormsModule} from '@angular/forms';
+import {Token} from '../../../../models/interfaces/token';
 
 @Component({
   selector: 'app-login',
   imports: [
     Header,
-    RemoveDashPipe,
-    Footer
+    Footer,
+    FormsModule
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  authService: Auth = inject( Auth );
+  router: Router = inject(Router);
+
+  token: string | null = localStorage.getItem('token') ? localStorage.getItem('token') : null;
+
+  loginObj: loginFromType = {
+    email: "",
+    password: ""
+  }
+
+  onLoginSubmit (form: FormsModule) {
+    this.authService.authenticateUser( this.loginObj ).subscribe({
+      next: (res: Token) => {
+
+        localStorage.setItem('token', res.token)
+        this.token = res.token;
+
+        console.log(res)
+
+        this.loginObj = {
+          email: "",
+          password: ""
+        }
+      },
+      error: (err) => {
+        console.log( err )
+      }
+    })
+  }
 
 }
