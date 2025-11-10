3d7b9ebb0c9c832f0af1ff2069c6d7dccbe84232
oussamasine07 <sineoussama@gmail.com>
2025-11-10 12:40:08 +0100

    make token functions

---
+++ frontend/src/app/models/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  firstName: string,
+  lastName: string,
+  role: string,
+  id: number,
+  email: string,
+  sub: string,
+  iat: number,
+  exp: number
+}
--- frontend/src/app/services/auth/auth.ts
+++ frontend/src/app/services/auth/auth.ts
@@ -3,6 +3,8 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {catchError, Observable, throwError} from 'rxjs';
 import {Token} from '../../models/interfaces/token';
 import {loginFromType} from '../../models/types/loginFormType';
+import {User} from '../../models/interfaces/user';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +19,29 @@ export class Auth {
         return throwError(() => err);
       })
     );
+  }
+
+  // get decoded token from the localstorage
+  getDecodedToken ( token: string | null ): User | null {
+    return token ? jwtDecode<User>( token ) : null;
+  }
+
+  // check if token is expired
+  isTokenExpired ( token: string | null ): boolean {
+    if (token) {
+      const decoded: any = jwtDecode(token)
+      const decodedExpDate = decoded.exp;
+      const currentDate = Date.now() / 1000;
+
+      return decodedExpDate < currentDate;
+    }
+    return true;
+  }
+
+  // get user role from token
+  getUserRole ( token: string | null ) {
+    const decoded: User | null = this.getDecodedToken(token);
+    return decoded ? decoded.role : null;
   }
 
 }
